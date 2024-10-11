601b297f503fdc2e1a110ba665d81d19fc5cea65
HarukiMikawa <hmm0032@uah.edu>
2024-10-11 10:02:35 -0500

    Update StratoSAT_BillyBob2024_MainBranch.ino

--- StratoSAT_BillyBob2024_MainBranch/StratoSAT_BillyBob2024_MainBranch.ino
+++ StratoSAT_BillyBob2024_MainBranch/StratoSAT_BillyBob2024_MainBranch.ino
@@ -7,3 +7,37 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+//Parachute Code
+void setup() {
+holdParachute();
+currentAltitude = GetAltitude();
+previousAltitude = currentAltitude;
+}
+
+void setup() {
+    // Initialize variables and setup
+    Hold parachute;
+    if (altitude == x) {
+        // Start sequence
+    }
+}
+
+void loop() {
+    currentAltitude = GetAltitude();
+    previousAltitude = currentAltitude;
+
+    // Balloon ascending loop
+    ascending = (previousAltitude - currentAltitude <= x)
+    if (balloonIsAscending) {
+        Hold parachute;
+    }
+
+    // Balloon descending condition
+    descending = (previousAltitude - currentAltitude >= -x);
+    if (balloonIsDescending()) {
+        Release parachute;
+        // End sequence or perform further actions
+    }
+}
+
