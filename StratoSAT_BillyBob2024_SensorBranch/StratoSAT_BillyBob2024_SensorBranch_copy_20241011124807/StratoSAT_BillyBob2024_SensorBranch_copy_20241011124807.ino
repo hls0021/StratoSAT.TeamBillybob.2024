9e247b98bc04c2af2d0e1d5df35b78a703220ad9
hls0021 <hls0021@uah.edu>
2024-10-19 16:55:05 -0500

    Update StratoSAT_BillyBob2024_SensorBranch_copy_20241011124807.ino

--- StratoSAT_BillyBob2024_SensorBranch/StratoSAT_BillyBob2024_SensorBranch_copy_20241011124807/StratoSAT_BillyBob2024_SensorBranch_copy_20241011124807.ino
+++ StratoSAT_BillyBob2024_SensorBranch/StratoSAT_BillyBob2024_SensorBranch_copy_20241011124807/StratoSAT_BillyBob2024_SensorBranch_copy_20241011124807.ino
@@ -1,30 +1,26 @@
+#include <math.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <Adafruit_BMP3XX.h>
-<<<<<<< HEAD
-#include <SparkFun_u-Blox_NEO_M9N.h>
-#include <SAMD21_Dev_Breakout.h>
-=======
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
 #include <utility/imumaths.h>
-#include <SPI.h>
-#include <SD.h>
->>>>>>> 1907a82c7de502d894e4841595df6784b2773f9a
+#include <string.h>
 
 // BNO055 sensor object
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 // GPS object 
-Adafruit_GPS GPS(&Serial1);
+SFE_UBLOX_GNSS GPS;
 // Declare global variables and constants 
 unsigned long startTime; 
 unsigned long currentTime; 
 int totalPackets = 1; 
-const char* teamID = "BillyBob"; //Subject to change
+String teamID = "BillyBob";
 void setup() {
   
   Serial.begin(115200);
-
-  while (!Serial) delay(10);  // wait for serial port to open!
+  Serial1.begin(115200);
+  while (!Serial1) delay(10);  // wait for serial port to open!
   
   if (!bno.begin())
   {
@@ -34,67 +30,32 @@ void setup() {
 
   delay(1000);
 
-	//serial communication
-	Serial.begin(9600);
-	Serial1.begin(9600);
 //GPS
 
-// Initialize SD card
-    if (!SD.begin(chipSelect)) {
-        Serial.println("SD card initialization failed!");
-        while (1);
-    }
-    Serial.println("SD card initialized.");
-
-// Open file to write data
-    dataFile = SD.open("datalog.txt", FILE_WRITE);
-    if (dataFile) {
-        dataFile.println("Payload Data Logging Started");
-        dataFile.println("Team ID: " + String(teamID));
-        dataFile.close();
-    }
 
 }
 
 
 
 void loop() {
+  startTime = millis();
+  //collect and output absolute orientation by euler angle
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
 
-delay(50)
-//collect and output absolute orientation by euler angle
-imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  //collect and output angular velocity with the gyro
+  imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
 
- //collect and output angular velocity with the gyro
-imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  // Collect and output acceleration
+  imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
 
-// Collect and output acceleration
-imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  //collect and output temperature
+  int temp = bno.getTemp();
 
-//collect and output temperature
-int = bno.getTemp()
-
-// gather and output GPS data
-?????????
+  // gather and output GPS data
  
+  if(millis() - startTime < 50) {
+    delay(50 - (millis() - startTime));
+  }
 }
 
-//log data
-dataFile = SD.open("datalog.txt", FILE_WRITE);
-        if (dataFile) {
-           // Output Euler angles (Orientation)
-            dataFile.print("Orientation (Euler angles): ");
-            // Output Gyroscope data
-            dataFile.print("Angular Velocity (Gyro): ");
-            // Output Acceleration
-            dataFile.print("Acceleration: ");
-            // Output Temperature
-            dataFile.print("Temperature: ");
-            // Output GPS data
-            dataFile.print("GPS Location: ");
-            // Output Time spent
-            dataFile.print("Time spent since start: ");
-            dataFile.print("UTC Time: ");
-            } else {
-            Serial.println("Error opening datalog.txt");
-        }
 
