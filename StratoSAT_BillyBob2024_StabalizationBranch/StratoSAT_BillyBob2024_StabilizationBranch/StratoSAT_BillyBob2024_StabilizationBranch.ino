4e647836bc310a427754b2c968a25bdae884ffb9
hls0021 <hls0021@uah.edu>
2024-10-19 17:14:13 -0500

    Update StratoSAT_BillyBob2024_StabilizationBranch.ino

--- StratoSAT_BillyBob2024_StabalizationBranch/StratoSAT_BillyBob2024_StabilizationBranch/StratoSAT_BillyBob2024_StabilizationBranch.ino
+++ StratoSAT_BillyBob2024_StabalizationBranch/StratoSAT_BillyBob2024_StabilizationBranch/StratoSAT_BillyBob2024_StabilizationBranch.ino
@@ -1,29 +1,14 @@
-Struct PID
-{
+
   float Kp;
   float Ki;
   float Kd;
-  float integral
-  float derivative
-  float integrallimit
-  float reference
-  a = 0.5
-  currentfilter = 0;
-  previousfilter = 0;
+  float integral;
+  float derivative;
+  float integrallimit;
+  float reference;
   lasterror = 0;
-  lastrefference = reference
-};
+  lastrefference = reference;
 
-while (Blank = blank) {
-    //Low pass Filter Implimetation
-  currentfliter = (a * previousfilter) + (1-a) * errordifference
-  previousfilter = currentfilter
-  //Integral and Derivative terms 
-  integral = integral + error * millis()
-  derivative = (error - lasterror) / millis()
-  if ()
-  error = lasterror
-}
 
 void setup() {
   // put your setup code here, to run once:
@@ -32,7 +17,11 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  //Integral and Derivative terms 
+  integral = integral + error * 50;
+  derivative = (error - lasterror) / 50;
+  if ()
+  error = lasterror;
 
 
 }
