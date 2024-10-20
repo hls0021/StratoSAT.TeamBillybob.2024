66e848aeb4b189a909e841039aaa463058b41c7c
hls0021 <hls0021@uah.edu>
2024-10-19 21:04:11 -0500

    Update StratoSAT_BillyBob2024_StabilizationBranch.ino

--- StratoSAT_BillyBob2024_StabalizationBranch/StratoSAT_BillyBob2024_StabilizationBranch/StratoSAT_BillyBob2024_StabilizationBranch.ino
+++ StratoSAT_BillyBob2024_StabalizationBranch/StratoSAT_BillyBob2024_StabilizationBranch/StratoSAT_BillyBob2024_StabilizationBranch.ino
@@ -3,6 +3,7 @@
   float Ki;
   float Kd;
   float integral;
+  float maxintegral
   float derivative;
   float integrallimit;
   float reference;
@@ -17,11 +18,18 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // Proportionalterm
+  blank
+  error = reference - blank
   //Integral and Derivative terms 
   integral = integral + error * (50 - (millis() - startTime));
+  if(integral > maxintegral) {
+    integral = 
+  }
   derivative = (error - lasterror) / (50 - (millis() - startTime));
+  output = (Kp * error) + (Ki * integral) + (Kd * derivative);
   if ()
-  error = lasterror;
+  lasterror = error;
 
 
 }
