be66c7f1f98f99abe4a4ed9550af9d2254eee0ff
HarukiMikawa <hmm0032@uah.edu>
2024-10-19 20:40:14 -0500

    Update StratoSAT_BillyBob2024_SensorBranch_copy_20241011124807.ino

--- StratoSAT_BillyBob2024_SensorBranch/StratoSAT_BillyBob2024_SensorBranch_copy_20241011124807/StratoSAT_BillyBob2024_SensorBranch_copy_20241011124807.ino
+++ StratoSAT_BillyBob2024_SensorBranch/StratoSAT_BillyBob2024_SensorBranch_copy_20241011124807/StratoSAT_BillyBob2024_SensorBranch_copy_20241011124807.ino
@@ -21,12 +21,12 @@ String teamID = "BillyBob";
 void setup() {
   
   Serial.begin(115200);
-  Serial1.begin(115200);
-  while (!Serial1) delay(10);  // wait for serial port to open!
+  Serial5.begin(115200);
+  while (!Serial5) delay(10);  // wait for serial port to open!
   
   if (!bno.begin())
   {
-    Serial.print("No BNO055 detected");
+    Serial5.print("No BNO055 detected");
     while (1);
   }
 
@@ -38,62 +38,59 @@ void setup() {
 
     //GPS
     if (GPS.begin()) {
-        Serial.println("u-blox GNSS module initialized successfully!");
+        Serial5.println("u-blox GNSS module initialized successfully!");
     } else {
-        Serial.println("u-blox GNSS module initialization failed!");
+        Serial5.println("u-blox GNSS module initialization failed!");
         while (1);  // If GNSS doesn't initialize, stop the program
     }
     //Start Time
-      startTime = millis();
+      startingTime = millis();
 }
 
 
 
 void loop() {
+    startTime = millis();
   //collect and output absolute orientation by euler angle
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-        Serial1.print("Orientation (Euler angles): ");
-        Serial1.print("Heading: "); Serial1.print(euler.x());
-        Serial1.print(" Roll: "); Serial1.print(euler.y());
-        Serial1.print(" Pitch: "); Serial1.println(euler.z());
+        Serial5.print("Orientation (Euler angles): ");
+        Serial5.print("Heading: "); Serial5.print(euler.x());
+        Serial5.print(" Roll: "); Serial5.print(euler.y());
+        Serial5.print(" Pitch: "); Serial5.println(euler.z());
   //collect and output angular velocity with the gyro
   imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-        Serial1.print("Angular Velocity (Gyro): ");
-        Serial1.print(gyro.x()); Serial1.print(", ");
-        Serial1.print(gyro.y()); Serial1.print(", ");
-        Serial1.print(gyro.z()); Serial1.println(" rad/s");
+        Serial5.print("Angular Velocity (Gyro): ");
+        Serial5.print(gyro.x()); Serial5.print(", ");
+        Serial5.print(gyro.y()); Serial5.print(", ");
+        Serial5.print(gyro.z()); Serial5.println(" rad/s");
   // Collect and output acceleration
   imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-        Serial1.print("Acceleration: ");
-        Serial1.print(accel.x()); Serial1.print(", ");
-        Serial1.print(accel.y()); Serial1.print(", ");
-        Serial1.print(accel.z()); Serial1.println(" m/s^2");
+        Serial5.print("Acceleration: ");
+        Serial5.print(accel.x()); Serial5.print(", ");
+        Serial5.print(accel.y()); Serial5.print(", ");
+        Serial5.print(accel.z()); Serial5.println(" m/s^2");
 
   //collect and output temperature
   int temp = bno.getTemp();
 
   // Collect and output GPS data 
         if (GPS.getLatitude() != 0 && GPS.getLongitude() != 0) {
-            Serial1.print("GPS Location: ");
-            Serial1.print(GPS.getLatitude() / 10000000.0, 6);  // Latitude
-            Serial1.print(", ");
-            Serial1.print(GPS.getLongitude() / 10000000.0, 6);  // Longitude
-            Serial1.println();
+            Serial5.print("GPS Location: ");
+            Serial5.print(GPS.getLatitude() / 10000000.0, 6);  // Latitude
+            Serial5.print(", ");
+            Serial5.print(GPS.getLongitude() / 10000000.0, 6);  // Longitude
+            Serial5.println();
         }
 
         if (GPS.getAltitude() != 0) {
-            Serial1.print("Altitude: ");
-            Serial1.print(GPS.getAltitude() / 1000.0);  // Altitude in meters
-            Serial1.println(" m");
+            Serial5.print("Altitude: ");
+            Serial5.print(GPS.getAltitude() / 1000.0);  // Altitude in meters
+            Serial5.println(" m");
         }
        
   // Gather and output time spent and UTC time
         currentTime = millis();
-        unsigned long timeSpent = currentTime - startTime;
-        Serial1.print("Time spent since start: ");
-        Serial1.print(timeSpent / 1000);  // in seconds
-        Serial1.println(" seconds");
- 
+        unsigned long timeSpent = currentTime - starting
   if(millis() - startTime < 50) {
     delay(50 - (millis() - startTime));
   }
