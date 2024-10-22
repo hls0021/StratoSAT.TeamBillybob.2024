5b5c654186d66d9cc390b7c7b1b2e358dbc14f70
hls0021 <hls0021@uah.edu>
2024-10-21 19:57:49 -0500

    Update StratoSAT_BillyBob2024_StabilizationBranch.ino

--- StratoSAT_BillyBob2024_StabalizationBranch/StratoSAT_BillyBob2024_StabilizationBranch/StratoSAT_BillyBob2024_StabilizationBranch.ino
+++ StratoSAT_BillyBob2024_StabalizationBranch/StratoSAT_BillyBob2024_StabilizationBranch/StratoSAT_BillyBob2024_StabilizationBranch.ino
@@ -5,18 +5,21 @@
   float integral;
   float derivative;
   float reference;
+  float lasterror = 0;
 
+  sensor_event_t event;
+  int solenoidclock = 14;
+  int solenoidcounter = 15;
 
 void setup() {
   // put your setup code here, to run once:
-  lasterror = 0;
-  lastrefference = reference;
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   // Proportional term
+  bno.getevent(&event);
   orienation = bno.getVector(Adafruit_BNO055::VECTOR_EULER); 
   error = reference - orientation;
   //Integral and Derivative terms 
@@ -27,18 +30,18 @@ void loop() {
   
   lasterror = error;
 
-  //The following is psuedocode, we need electrical to tell us where the solenoid mosfets are connected because we can't read schemetics
-  //Pins 14 and 15 (A0 and A1) are connected to the solenoids
-
   if(output >= setvalue) {
-    both open solenoids
+    digitalWrite(solenoidclock, HIGH);
+    digitalWrite(solenoidcounter, LOW);
   }
-  else {
-    if(output >= setvalue) {
-      alternate solenoids
-    }
-    else {
-      both closed solenoids
-    }
+  if(output <= setvalue) {
+    digitalWrite(solenoidclock, LOW);
+    digitalWrite(solenoidcounter, HIGH);
   }  
+  if(output >= negativezero) {
+    if(output <= positivezero) {
+      digitalWrite(solenoidclock, LOW);
+      digitalWrite(solenoidcounter, LOW);
+    }
+  }
 }
